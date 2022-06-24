ba64d4038ca10079f98168a424852c24f5a453d5
LeoContreras93 <Contreras.leo2211@gmail.com>
2022-06-24 09:16:52 -0700

    week1assesment

--- code-challenges.js
+++ code-challenges.js
@@ -1,123 +1,128 @@
-// ASSESSMENT 1: Coding Practical Questions
+// // ASSESSMENT 1: Coding Practical Questions
 
-// To run file: navigate to the correct directory in terminal, $ node code-challenges.js
+// // To run file: navigate to the correct directory in terminal, $ node code-challenges.js
 
-// Pseudo coding is required.
+// // Pseudo coding is required.
 
-// --------------------INSTRUCTOR EXAMPLE: Create a function that takes in any two strings and returns the string with more characters. Use the two sets of test variables provided.
+// // --------------------INSTRUCTOR EXAMPLE: Create a function that takes in any two strings and returns the string with more characters. Use the two sets of test variables provided.
 
-// Pseudo code:
+// // Pseudo code:
 
-// Set one:
-const fruit1 = "apple"
-const fruit2 = "banana"
+// // Set one:
+// const fruit1 = "apple"
+// const fruit2 = "banana"
 
-// Set two:
-const fruit3 = "cherry"
-const fruit4 = "kiwi"
+// // Set two:
+// const fruit3 = "cherry"
+// const fruit4 = "kiwi"
 
-const stringCombo = (string1, string2) => {
-    if(string1.length > string2.length) {
-      return string1
-    } else if(string1 < string2) {
-      return string2
-    } else {
-      return "Oops, neither are greater"
-    }
-  }
+// const stringCombo = (string1, string2) => {
+//     if(string1.length > string2.length) {
+//       return string1
+//     } else if(string1 < string2) {
+//       return string2
+//     } else {
+//       return "Oops, neither are greater"
+//     }
+//   }
   
-  console.log(stringCombo(fruit1, fruit2))
-  console.log(stringCombo(fruit3, fruit4))
-  //banana
-  //cherry
+//   console.log(stringCombo(fruit1, fruit2))
+//   console.log(stringCombo(fruit3, fruit4))
+//   //banana
+//   //cherry
 
 
-// --------------------1) Create a function that takes in any given number and determines if the number is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit. Use the test variables provided below. Expected output: "42 is below boiling point", "350 is above boiling point", "212 is at boiling point"
+// // --------------------1) Create a function that takes in any given number and determines if the number is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit. Use the test variables provided below. Expected output: "42 is below boiling point", "350 is above boiling point", "212 is at boiling point"
 
-// Pseudo code: so any of the 3 test variables i place in console will return an answer about the tempature being below,at or above boiling point. tempature is my special veriable and I am invoking the test variables in the console
+// // Pseudo code: input: 42, 350, 212 and Expected output: "42 is below boiling point", "350 is above boiling point", "212 is at boiling point"
+//    creating a function call hotWater, using an equality operator to determine if my variable is strictly equal to the data type or value.
 
+// const temp1 = 42
+// const temp2 = 350
+// const temp3 = 212
 
-const temp1 = 42
-const temp2 = 350
-const temp3 = 212
-
-const hotWater = (tempature) => {
-  if(tempature === 42) {
-    return "42 is below boiling point"
-  } else if(tempature === 212) {
-    return "212 is at boiling point"
-  } else if(tempature === 350) {
-    return "350 is above boiling point"
-  }
-}
-console.log(hotWater(42)) // 42 is below boiling point
-console.log(hotWater(212)) // 212 is at boiling point
-console.log(hotWater(350)) // 350 is above boiling point
-
-
-
-
-
-
-// --------------------2) Create the code that will combine the two arrays and return the length using the variables provided below. Expected output: 10
-
-// Pseudo code: concatenating two arrays together and invoking .length in the console to get the length of the two arrays.
-
-const myNumbers1 = [3, 7, 0, 36, -9]
-const myNumbers2 = [8, -7, 42, 9, 13]
-console.log(myNumbers1.length + myNumbers2.length) // 10
-
-
-// --------------------3) Create the code that will reverse the letters of a string using the test variable provided below. Expected output: "2202 atleD"
-
-// Pseudo code: I used the split built in method to make the string into an array of substrings then use the reverse built in method to reverse the order of the array i used split with and joined each individual letter together using .join built in method.
-
-const currentCohort = "Delta 2022"
-var cohort = currentCohort.split("")
-console.log(cohort.reverse().join("")) // 2202 atleD
-
-
-
-// --------------------4) Create a statement that will evaluate whether each number in an array is even or odd. Use the test variable provided below. Expected output: "odd" "even" "odd" "even" "odd" "even"
-
-// Pseudo code: unable to get the result of odd even, tried creating global variable with odd and even 
-
-const myArray = [13, 34, 5, 10, 27, 42]
-for(let i = 0; i < myArray.length; i++) {
-  if(myArray % 2 !== 0) {
-    console.log(`${myArray[0]} odd`)
-  } else if(myArray % 2 !== 0) {
-    return "even"
-  }
-}
-console.log(myArray[0])
+// const hotWater = (tempature) => {
+//   if(tempature === 42) {
+//     return "42 is below boiling point"
+//   } else if(tempature === 212) {
+//     return "212 is at boiling point"
+//   } else if(tempature === 350) {
+//     return "350 is above boiling point"
+//   }
+// }
+// console.log(hotWater(42)) // 42 is below boiling point
+// console.log(hotWater(212)) // 212 is at boiling point
+// console.log(hotWater(350)) // 350 is above boiling point
 
 
 
 
 
-     
+
+// // --------------------2) Create the code that will combine the two arrays and return the length using the variables provided below. Expected output: 10
+
+// // Pseudo code: concatenating two arrays together and invoking .length in the console to get the length of the two arrays.
+
+// const myNumbers1 = [3, 7, 0, 36, -9]
+// const myNumbers2 = [8, -7, 42, 9, 13]
+// console.log(myNumbers1.length + myNumbers2.length) // 10
+
+
+// // --------------------3) Create the code that will reverse the letters of a string using the test variable provided below. Expected output: "2202 atleD"
+
+// // Pseudo code: I used the split built in method to make the string into an array of substrings then use the reverse built in method to reverse the order of the array i used split with and joined each individual letter together using .join built in method.
+
+// const currentCohort = "Delta 2022"
+// var cohort = currentCohort.split("")
+// console.log(cohort.reverse().join("")) // 2202 atleD
 
 
 
-// --------------------5) Create a function that takes in a set of any two numbers and subtracts the smaller number from the larger number using the sets of test variables provided below. Expected output: 42, 3
+// // --------------------4) Create a statement that will evaluate whether each number in an array is even or odd. Use the test variable provided below. Expected output: "odd" "even" "odd" "even" "odd" "even"
 
-// Pseudo code: I am not sure how to set this up where i put in 58-100 to show 42 and vice versa
+// // Pseudo code: using the created array, using a for loop to intizialize my variable let and using an index of 0 to start from the beginning of my array to the end of it using .length built in method which counts all characters in an array. incrementing by one for each iteration. modulo to determined a remainder and using logical and equality operators. still unsure of the answer in this question
 
-// Set one:
-const number1 = 58
-const number2 = 100
+// const myArray = [13, 34, 5, 10, 27, 42]
+// for(let i = 0; i < myArray.length; i++) {
+//   if(myArray % 2 !== 0) {
+//     console.log(`${myArray[0]} odd`)
+//   } else if(myArray % 2 !== 0) {
+//     return "even"
+//   }
+// }
+// console.log(myArray[0])
 
-// Set two:
-const number3 = 27
-const number4 = 24
+// const myArray = [13, 34, 5, 10, 27, 42]
+// for(i=0; i<myArray.length; i++) {
+//     if(myArray[i] % 2 === 0) {
+//         console.log("even")
+//     } else
+//         console.log("odd")
+// }
+// console.log(myArray)
 
-const numCombo = (number1, number2, number3, number4) => {
-  if(number2 - number1) {
-    return 42
-  } else if(number3 - number4) {
-    return 3
-  }
-}
-console.log(number2 - number1)
-console.log(number3 - number4)
+
+
+// // --------------------5) Create a function that takes in a set of any two numbers and subtracts the smaller number from the larger number using the sets of test variables provided below. Expected output: 42, 3
+
+// // Pseudo code: my inputs are set one and set two , my outcome is 42 and 3. declared a new variable assigning it it my inputs. using an if statement to pass an evaluation to determine my output.
+
+// // Set one:
+// const number1 = 58
+// const number2 = 100
+
+// // Set two:
+// const number3 = 27
+// const number4 = 24
+
+// const numCombo = (number1, number2, number3, number4) => {
+//   if(number2 - number1) {
+//     return 42
+//   } else if(number3 - number4) {
+//     return 3
+//   }
+// }
+// console.log(number2 - number1)
+// console.log(number3 - number4)
+
+
--- interview-questions.md
+++ interview-questions.md
@@ -7,67 +7,55 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 INSTRUCTOR EXAMPLE: What is a conditional statement?
 
-  Your answer: true or false statements using if, else and else if
-  Researched answer: Conditional statements control behavior in JavaScript and determine whether or not pieces of code can run.
-
-
-
+  Your answer: true or false statements using if, else and else if. they create a desired output if certain criteria is met. they change the behavior of data.
+  Researched answer: they are decision makers, create a preferred outcome, they help solve problems with multiple inputs and can be useful in structuring an outcome.
 
 
 1. What is git? What is the difference between git and Github?
 
   Your answer: git is version control used on your local machine to send or receive information from the cloud(github). github is a website which contains repositiories where one can store their code for everyone to see.
 
-  Researched answer: Simply put, Git is a version control system that lets you manage and keep track of your source code history. GitHub is a cloud-based hosting service that lets you manage Git repositories
-
+  Researched answer: github is open source and can be used to create repositories and store code as well as have code or files uploaded and be made available to others. git is a version control software available on your own pc, you are able to clone or create repositories(folders). it also helps you keep track of anyc changes you made and manage code.
 
 
 2. Which JavaScript operators will return a Boolean value?
 
-  Your answer: Equality Operators, there are three types, loose equality, strict equality and logical operator
-
-  Researched answer: Boolean values are very useful when making evaluations about data. Equality operators compare one value to the other and judge if the values equate to the same thing. If they do equate to the same a Boolean value of true is returned if not we get a Boolean value of false. There are two types of equality operators, the loose equality operator and the strict equality operator. The loose equality operator converts the two items to the same data type before making a comparison and the strict equality operator requires the two items being compared must be the same data type to return true.
-
+  Your answer: Equality Operators return a boolean value of true or false, there are three types, loose equality which is == and checks only the value(not the prefferred method), strict equality checks both data type and value are equal to each other and relational operator <(less than) or >(greater than)return an outcome of true or false from comparing two things. logical operators &&(and), ||(or), !(not) are use to determine the logic between variables or values.
+  Researched answer: the js operators that will return a boolean value are logical operators, equality operators or comparison operators also return a boolean value which is true or false. these operators can also be used in conditional statements. you can have two true statements but javascript will only run the first one.
 
 
 3. What is an index? What is the difference between index and value?
 
-  Your answer: index is like the number placement order of things, value is a result of something thats being declared.
-
-  Researched answer: The index property is a read-only property. It contains the position of a regular expression match within a string.
-
-
-
+  Your answer: index is like the number placement order within an array, value can be assinged to a variable that is being declared using the assignment operator = 
+  Researched answer: index is the position of a value assinged on to an array for example. all arrays are indexed no matter the data type. computers use zero based indexing to count starting from 0-10 and so on. it is good practice to declare all variables at the beginning of your code and then assign values to them using the assignmet operator which does not evaluate equality.
 
 
 4. What is iteration?
 
-  Your answer: something that repeats?
+  Your answer: the repition of a process until a certain condition is met which will determine the output. iteration is used in for loops which are great for counting.
 
-  Researched answer: In JavaScript an iterator is an object which defines a sequence and potentially a return value upon its termination.
-
-
+  Researched answer: the process of repeating something over and over again, requires you to initialize a variable then add a condition which will be evaluated before each iteration and you can increment or decrement.
 
 5. Give a brief description of proper pair programming techniques. What are the roles of each person?
 
-  Your answer: one is a driver who writes code and the other is a researcher,proof reader 
+  Your answer: your driver is typing and listening extremely hands on. Navigator gives directions, is observing for any potential mistakes and has clear path to what is coming next. 
 
-  Researched answer: higher quality code and faster delivery of it, knowledge sharing, helps build relationships, team morale, helps minimize distractions, brainstorming, allows you to switch roles.
-
+  Researched answer: code quality, focus, accountability, brainstorming ideas, knowledge-sharing, good morale, less mental fatigue.
 
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. Higher Order Functions: a function that can be assinged to a variable same way arrays or strings are assigned to variables and returns another function when called upon.
+1. Higher Order Functions: a function that can be assinged to a variable same way arrays or strings are assigned to variables and returns another function when called upon. a function within a function.
 
-2. Jest: its a open source testing framework and works with React.
+2. Jest: its a open source testing framework and works with programs like React. Test driven devolopement, allows you to test your code using the describe built-in-method. will check for any errors in your code prior to writing it in its entirety.
 
-3. Objects: like storage containrers that hold data
+3. Objects: like storage containrers that hold data, you can have data types nested in objects and are called upon using dot notation
 
-4. Method: it stores property and can be used in a function?
+4. Method: JavaScript methods are actions that can be performed on objects using a function which use built in methods like this.
 
-5. Classes: they are used with objects, they can also be customized like functions and are reusable.
+5. Classes:A JavaScript class is not an object. It is a template for JavaScript objects, class methods are created with the same syntax as object methods.
+
 
 
